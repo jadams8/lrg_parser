e000b010df3a4f5e6c7f7eeed9a447307bb9a443
Nana Mensah <Nana.mensah1@nhs.net>
2018-11-22 11:42:10 +0000

    Move script actions to a main function

--- lrg_parser.py
+++ lrg_parser.py
@@ -1,10 +1,6 @@
 import xml.etree.ElementTree as ET
 import os, argparse
 
-parser = argparse.ArgumentParser(description='Parse an XML file to produce a BED file')
-parser.add_argument('xml_file', help='Enter the name of the LRG file to parse')
-args = parser.parse_args()
-
 
 def lrg_parse(xml_file):
 	"""function will parse the LRG file to extract the exon number and LRG co-ordinates for the start and end of each codon"""
@@ -23,13 +19,21 @@ def lrg_parse(xml_file):
 					data_list.append([exon.attrib['label'], child.attrib['start'], child.attrib['end']])
 	return data_list
 
-# store the output from lrg_parse() in a variable to write to a file
-data = lrg_parse(args.xml_file) 
+def main():
+	parser = argparse.ArgumentParser(description='Parse an XML file to produce a BED file')
+	parser.add_argument('xml_file', help='Enter the name of the LRG file to parse')
+	args = parser.parse_args()
 
-file_name = args.xml_file.split('.')[0]+'.bed'
+	# store the output from lrg_parse() in a variable to write to a file
+	data = lrg_parse(args.xml_file) 
 
-with open(file_name, 'w') as myfile:
-	for my_list in data:
-		# reformat the list to tab seperated and different lines
-		new_list = "\t".join(my_list) + '\n'
-		myfile.write(new_list)
+	file_name = args.xml_file.split('.')[0]+'.bed'
+
+	with open(file_name, 'w') as myfile:
+		for my_list in data:
+			# reformat the list to tab seperated and different lines
+			new_list = "\t".join(my_list) + '\n'
+			myfile.write(new_list)
+
+if __name__ == "__main__":
+	main()
