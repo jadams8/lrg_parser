722ecb59de06a4323704dcb9172aacaafa997e8f
Jes <jes_adams@hotmail.co.uk>
2018-11-22 17:08:06 +0000

    fix matching transcript bug, add comments

--- lrg_parser.py
+++ lrg_parser.py
@@ -3,6 +3,7 @@ import os, argparse, fnmatch, re, requests
 
 
 def parse_file(xml_file):
+	'''Will set the root from an XML file already stored on the system'''
 	lrg = ET.parse(xml_file)
 	root = lrg.getroot()
 	return root
@@ -16,14 +17,18 @@ def lrg_parse(root):
 		if 'label' in exon.attrib.keys():
 			for child in exon:
 				# make sure the only co-ordinates printed are the LRG co-ordinates
-				if fnmatch.filter(child.attrib.values(), 'LRG_?'):
+				if list(root.iter('id'))[0].text == child.attrib['coord_system']:
+					# create a list of data that needs to be in the bed file
 					data_to_append = (exon.attrib['label'], child.attrib['start'], child.attrib['end'])
+					# only append data that is not already in the list to prevent duplications
 					if data_to_append not in data_list:
 						data_list.append(data_to_append)
+	# sort the data according to position in the genome
 	sorted_data = sorted(data_list, key=lambda line: int(re.split('[A-Za-z]+', line[0])[0]))
 	return convert_coords(root, sorted_data)
 
 def write_file(data, file_name):
+	'''write the data from parsing into a new bed file with the same name as the XML file'''
 	with open(file_name, 'w') as myfile:
 		for my_tuple in data:
 			# reformat the list to tab seperated and different lines
@@ -31,8 +36,11 @@ def write_file(data, file_name):
 			myfile.write(my_list)
 
 def get_file(lrg_num):
+	'''if the file is not already on the system get the file from the LRG website'''
 	website = 'http://ftp.ebi.ac.uk/pub/databases/lrgex/'+ lrg_num +'.xml'
 	xml_text = requests.get(website)
+	# LRGs pending approval will have a different URL, so if the above web
+	# address throws a 404 error then use a different URL with 'pending' in
 	if xml_text.status_code == 404:
 		website = 'http://ftp.ebi.ac.uk/pub/databases/lrgex/pending/'+ lrg_num +'.xml'
 		xml_text = requests.get(website)
@@ -54,12 +62,10 @@ def convert_coords(xml_root, sorted_data):
 		genome_loc1 = int(grch38_map['other_start'])
 		genome_loc2 = int(grch38_map['other_end'])
 		exon_label, lrg_start_pre, lrg_end_pre = xml_tuple
-		print(xml_tuple)
 		# Set LRG strat and end coordinates ( -1 for addition/subtraction from reference genome )
 		lrg_start = int(lrg_start_pre) - 1
 		lrg_end = int(lrg_end_pre) -1
 		# If reverse strand, swap genome start and end, multiply lrg coordinates by -1 to subtract
-		print(grch38_map['strand'])
 		if grch38_map['strand'] == '-1':
 			genome_loc1, genome_loc2 = genome_loc2, genome_loc1
 			lrg_start, lrg_end = map(lambda x: x*-1, [lrg_start, lrg_end])
@@ -69,7 +75,6 @@ def convert_coords(xml_root, sorted_data):
 			str(genome_loc1 + lrg_start),  # grch38 start coordinates
 			str(genome_loc1 + lrg_end)  # grch38 end coordinates
 		)
-		print(coords)
 		output.append(coords)
 	return output
 
@@ -82,6 +87,8 @@ def main():
 	# store the output from lrg_parse() in a variable to write to a file
 	files_to_parse = args.xml_file
 	for f in files_to_parse:
+		# call different functions depending on whether the XML file is 
+		# on the system or needs to be accessed from the web
 		if os.path.isfile(f):
 			data = lrg_parse(parse_file(f))
 		else:
