eec928816e7d0e08c0b354c22c75140897c86d2b
Nana Mensah <Nana.mensah1@nhs.net>
2018-12-02 10:09:18 +0000

    Update project requirements in notes.txt. Definte tests to write in tests.py.

--- tests.py
+++ tests.py
@@ -1,22 +1,15 @@
-"""Tests for lrg_parser"""
-import os, subprocess
+"""Tests for lrg_parser
+
+### Project test checklist
+test_LRG_input - Reads LRG XML file as a string?
+test_LRG_web_input - Gets *accurate* LRG data from internet as a string?
+test_set_root - Turns LRG XML data into xml.Elemtree root object for parsing?
+test_extract - Gets exon regions from root? Compare output of lrg_parse with test data.
+test_convert - Converts exon regions to coordinates? Compare output of conver_coords with test data.
+test_write - Assert that function uses python write function
+test_BED - Assert that output file is in BED format (tab-delimited records). Use test data.
+"""
+
+import os
 import lrg_parser
 
-def test_creates_file():
-    """Test that it creates a new file from an XML input"""
-    # Call from command line
-    subprocess.call(['python', 'lrg_parser.py', 'data/LRG_1.xml'])
-    # Assert that a file was output
-    assert os.path.isfile('data/LRG_1.bed')
-
-def test_all_exons():
-    """Test all LRG_5 exon labels are present in first column of lrg_parser output"""
-    # Create a list of exon labels expected in LRG_5
-    exon_labels = [str(number) for number in range(1, 14)]
-    exon_labels.extend(['14a', '14b', '14c', '15'])
-    # Create a list of exons parsed from LRG_5
-    parsed_data = lrg_parser.lrg_parse('data/LRG_5.xml')
-    parsed_exons = set([item[0] for item in parsed_data])
-    # Test that all expected exon labels are present
-    for exon_label in exon_labels:
-        assert exon_label in parsed_exons
